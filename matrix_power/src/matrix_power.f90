cdc7999ce2fb9a33add4c052857ae09f31114239
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-03-29 17:14:45 -0700

    print final matrix

--- matrix_power/src/matrix_power.f90
+++ matrix_power/src/matrix_power.f90
@@ -2,12 +2,24 @@ program allocatable
     implicit none
     ! variable declarations
     integer :: matrix_dimension
-    character :: is_id
     real, allocatable :: matrix(:, :)
+    real, allocatable :: matrix_power(:, :)
+    character :: is_id
+    integer :: power
+    ! loop iterators
+    integer :: i
+    integer :: j
 
     ! get user defined matrix dimension
     print *, "Matrix dimension: " ! TODO: add error checking
     read(*, *) matrix_dimension
+    ! allocate matrix memories
+    allocate(matrix(matrix_dimension, matrix_dimension)) 
+    allocate(matrix_power(matrix_dimension, matrix_dimension))
+    
+    ! get user defined power
+    print *, "Power: "
+    read(*, *) power
 
     ! check if identity matrix should be used
     print *, "Use identity matrix? (y/n) " ! TODO: add error checking
@@ -19,13 +31,22 @@ program allocatable
         ! make matrix random
     end if
 
-    ! create initial matrix
-    allocate(matrix(matrix_dimension, matrix_dimension)) 
+    call random_number(matrix) ! initialize matrix with random numbers
 
-    call random_number(matrix)
-    print *, matrix
+    matrix_power = matrix ! initialize matrix_power as the matrix itself.
 
+    ! multiply the matrix by itself power times
+    do i = 1, power - 1
+        matrix_power = matmul(matrix, matrix_power)
+    end do
+
+    ! print matrix_power to the screen
+    do j = 1, matrix_dimension
+        print *, matrix_power(j, :)
+    end do
+
+    ! free memory used by matrices
+    deallocate(matrix_power)
     deallocate(matrix)
-    print *, "got through everything!"
 end program allocatable
 
