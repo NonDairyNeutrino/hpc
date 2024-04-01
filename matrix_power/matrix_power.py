bf59c10454930ffb62eb12577d82af7ea39a448b
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-04-01 10:43:23 -0700

    fix matrix power

--- matrix_power/matrix_power.py
+++ matrix_power/matrix_power.py
@@ -5,7 +5,7 @@ def matrix_multiply(A, B):
     # multiply two matrices
     # inputs: two matrices
     # outputs: the product of the input matrices
-    product = cp.copy(A)
+    product = [[0 for j in range(len(B))] for i in range(len(A))]
     for i in range(len(A)):
         for j in range(len(B[1])):
            product[i][j] = sum(A[i][k] * B[k][j] for k in range(len(B)))
@@ -21,5 +21,5 @@ def matrix_power(p, A):
         return power
 
 p = int(sys.argv[1])
-matrix = [[2,0,0],[0,2,0],[0,0,2]])
-print(matrix_power(p, matrix)
+matrix = [[2,0,0],[0,2,0],[0,0,2]]
+print(matrix_power(p, matrix))
