0b183ace27e63f2cb6958248f36a496fef8ec8bc
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-04-07 14:44:59 -0700

    add Doxygen support

--- matrix_power/build.sh
+++ matrix_power/build.sh
@@ -3,24 +3,32 @@
 # COLORS
 RED='\033[0;31m'
 BLUE='\033[0;34m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
 # BUILD VARIABLES
 BUILD_PATH="build/"
 BUILD_LOG_PATH="${BUILD_PATH}build.log"
 BIN_PATH="bin/"
+DOCS_PATH="docs/"
 mkdir -p $BUILD_PATH
 mkdir -p $BIN_PATH
+mkdir -p $DOCS_PATH
 
 # BEGIN BUILD SCRIPT
-echo "STARTING BUILD PROCESS IN $BUILD_PATH"              > $BUILD_LOG_PATH
-cmake -B $BUILD_PATH                                     >> $BUILD_LOG_PATH # creates a Makefile in build/
-echo "BUILD FILES AND MAKEFILE GENERATED IN $BUILD_PATH" >> $BUILD_LOG_PATH 
-echo "GENERATING EXECUTABLE IN $BIN_PATH"                >> $BUILD_LOG_PATH
-make --directory $BUILD_PATH                            &>> $BUILD_LOG_PATH # creates the executable in bin/
+echo "BUILDING IN $BUILD_PATH"                                                > $BUILD_LOG_PATH
+cmake -B $BUILD_PATH                                                         >> $BUILD_LOG_PATH # creates a Makefile in build/
+echo "${GREEN}BUILD FILES AND MAKEFILE GENERATED IN ${BLUE}$BUILD_PATH${NC}" >> $BUILD_LOG_PATH 
+
+echo "GENERATING EXECUTABLE IN $BIN_PATH"                                    >> $BUILD_LOG_PATH
+make --directory $BUILD_PATH                                                &>> $BUILD_LOG_PATH # creates the executable in bin/
 if [ $? -eq 0 ]; then
-    echo "GENERATED EXECUTABLE IN $BIN_PATH"             >> $BUILD_LOG_PATH
+    echo "${GREEN}GENERATED EXECUTABLE IN ${BLUE}$BIN_PATH${NC}"             >> $BUILD_LOG_PATH
 else
-    echo -e "${RED}COMPILE ERROR; SEE ${BLUE}$BUILD_LOG_PATH${NC}"
+    echo -e "${RED}COMPILE ERROR; SEE ${BLUE}$BUILD_LOG_PATH${NC}"           >> $BUILD_LOG_PATH
 fi
+
+echo "GENERATING DOCUMENTATION IN $DOCS_PATH"                                >> $BUILD_LOG_PATH
+doxygen                                                                      >> $BUILD_LOG_PATH
+echo "${GREEN}DOCUMENTATION GENERATED IN ${BLUE}$DOCS_PATH${NC}"             >> $BUILD_LOG_PATH
 # END BUILD SCRIPT
--- matrix_power/src/io.f90
+++ matrix_power/src/io.f90
@@ -1,28 +1,31 @@
+!> @file io.f90
+!! @brief Module to define input and output functionality for matrix powers
 module io
     implicit none
-    private
-    public get_parameters
+    public
+
 contains
 
+    !> @brief Interactively get user-defined matrix dimension
+    !! @return User-defined matrix dimension
     function get_dimension() result (matrix_dimension)
-        ! get user defined matrix dimension
-        ! inputs: none
-        ! outputs: matrix_dimension
+        implicit none
         integer :: matrix_dimension
 
         print *, "Matrix dimension: "
-        read(*, *) matrix_dimension
+        read(*, *) matrix_dimension !> Define matrix dimension from stdin
 
+        !> Error checking to make sure input is a positive integer
         do while (matrix_dimension < 1)
             print *, "Please enter a positive integer"
             read(*,*) matrix_dimension
         end do
     end function get_dimension
 
+    !> @brief Interactively get user-defined matrix power
+    !! @return User-defined matrix power
     function get_power() result (power)
-        ! get user defined matrix power
-        ! inputs: none
-        ! outputs: power
+        implicit none
         integer :: power
     
          print *, "Power: "
@@ -34,14 +37,14 @@ contains
         end do
     end function get_power
 
+    !> @brief Interactively get whether or not the matrix should be diagonal
+    !! @return 1 or 0 if the matrix is diagonal or not, respectively
     function get_id() result (is_id)
-        ! get whether or not the matrix is the identity
-        ! inputs: none
-        ! outputs: 
+        implicit none
         character :: id_input
         integer   :: is_id
 
-        ! check if identity matrix should be used
+        !> check if identity matrix should be used
         print *, "Use identity matrix? (y/n) "
         read(*, *) id_input
 
@@ -57,10 +60,10 @@ contains
         end if
     end function get_id
 
-    function get_parameters() result (parameters)
-        ! get all parameters
-        ! inputs: none
-        ! outputs: matrix dimension, matrix power, if the matrix should be the identity
+    !> @brief Interactively get user-defined parameters
+    !! @return Array of parameters [matrix_dimension, matrix_power, is_id]
+    function get_parameters_interactive() result (parameters)
+        implicit none
         integer :: matrix_dimension
         integer :: power
         integer :: is_id
@@ -70,5 +73,27 @@ contains
         power            = get_power()
         is_id            = get_id()
         parameters       = [matrix_dimension, power, is_id]
-    end function get_parameters
+    end function get_parameters_interactive
+
+    !> @brief Get parameters from command line arguments
+    !! @return Array of parameters [matrix_dimension, matrix_power, is_id]
+    function get_parameters_cli() result (parameters)
+        implicit none
+        ! get parameters from command line arguments
+        ! inputs: none
+        ! outputs: matrix_dimension, power, is_id
+        character(len=4) :: args(3)
+        integer          :: parameters(3)
+        ! dummy variables
+        integer :: i
+        integer :: stat
+        
+        do i = 1, 3
+            call get_command_argument(i, args(i))
+        end do
+
+        read(args, *, iostat=stat) parameters
+        
+    end function get_parameters_cli
+
 end module io
