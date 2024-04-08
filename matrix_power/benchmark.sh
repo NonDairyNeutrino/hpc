78ce3c178285c69d63f18906d63ef7b3a49e46de
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-04-07 23:03:30 -0700

    format benchmark call

--- matrix_power/benchmark.sh
+++ matrix_power/benchmark.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-hyperfine "bin/matrix_power $1 $2 $3" "python src/matrix_power_python.py $1 $2"
+hyperfine \
+    --export-markdown benchmark.md\
+    --export-csv      benchmark.csv\
+    --min-runs 30\
+    --command-name "Fortran" "bin/matrix_power $1 $2 $3"\
+    --command-name "Python" "python src/matrix_power_python.py $1 $2"
