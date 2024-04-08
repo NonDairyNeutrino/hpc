b5779882b39d1bb9e071ceee78130d2610213b72
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-04-08 11:19:45 -0700

    update benchmark script

--- matrix_power/benchmark.sh
+++ matrix_power/benchmark.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "BEGINNING BENCHMARKS"
+
+mkdir -p bench
+
 hyperfine \
-    --export-markdown benchmark.md\
-    --export-csv      benchmark.csv\
-    --min-runs 30\
-    --command-name "Fortran" "bin/matrix_power $1 $2 $3"\
-    --command-name "Python" "python src/matrix_power_python.py $1 $2"
+    --export-markdown bench/benchmark.md\
+    --export-csv      bench/benchmark.csv\
+    --min-runs 30\ # Need at least 30 to be statistically significant i.e. central limit theorem
+    --parameter-list DIM 2,3,4,5,6,7,8,9,10\
+    --parameter-list POWER 2,3,4,5,6,7,8,9,10\
+    --command-name "Fortran - {DIM} - {POWER}" "bin/matrix_power {DIM} {POWER} $3"
+
+echo "BENCHMARKS FINISHED. RESULTS IN bench/"
