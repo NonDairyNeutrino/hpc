ec03d67196961dc3202d89103fd19f8f1ae43a91
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-05-08 19:05:42 -0700

    add matrixPower

--- matrix_power_multithreaded/src/matrix.jl
+++ matrix_power_multithreaded/src/matrix.jl
@@ -1,3 +1,6 @@
+using LinearAlgebra
+using BenchmarkTools
+
 # matrix multiplication
 function dot(vec1 :: Vector{Float64}, vec2 :: Vector{Float64}) :: Float64
     dot_product = 0
@@ -7,13 +10,13 @@ function dot(vec1 :: Vector{Float64}, vec2 :: Vector{Float64}) :: Float64
     return dot_product
 end
 
-function matrix_multiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threaded="none")
+function matrixMultiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threaded="none")
     # initialize product matrix
     product = Matrix{Float64}(undef, size(mat1)[1], size(mat2)[2])
     # choose behavior based on specified threading
     if threaded == "none"
         # do regular matrix multiplication
-        println("Proceeding with sequential matrix multiplication...")
+        # println("Proceeding with sequential matrix multiplication...")
         for i in 1:size(mat1)[1]
             for j in 1:size(mat2)[2]
                 product[i,j] = dot(mat1[i,:], mat2[:,j])
@@ -23,7 +26,7 @@ function matrix_multiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threa
     elseif threaded == "element"
         # matrix multiplication where each element of the product
         # is calculated in its own threaded
-        println("Proceeding with element-threaded matrix multiplication...")
+        # println("Proceeding with element-threaded matrix multiplication...")
         Threads.@threads for i in 1:size(mat1)[1]
             Threads.@threads for j in 1:size(mat2)[2]
                 product[i,j] = dot(mat1[i,:], mat2[:,j])
@@ -32,7 +35,7 @@ function matrix_multiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threa
     elseif threaded == "row"
         # matrix multiplication where each row of the product
         # is calculated in its own threaded
-        println("Proceeding with row-threaded matrix multiplication...")
+        # println("Proceeding with row-threaded matrix multiplication...")
         Threads.@threads for i in 1:size(mat1)[1]
             for j in 1:size(mat2)[2]
                 product[i,j] = dot(mat1[i,:], mat2[:,j])
@@ -41,7 +44,7 @@ function matrix_multiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threa
     elseif threaded == "column"
         # matrix multiplication where each column of the product
         # is calculated in its own threaded
-        println("Proceeding with column-threaded matrix multiplication...")
+        # println("Proceeding with column-threaded matrix multiplication...")
         for i in 1:size(mat1)[1]
             Threads.@threads for j in 1:size(mat2)[2]
                 product[i,j] = dot(mat1[i,:], mat2[:,j])
@@ -51,8 +54,18 @@ function matrix_multiply(mat1 :: Matrix{Float64}, mat2 :: Matrix{Float64}; threa
     return product
 end
 
-mat = Float64[2 0 0; 0 2 0; 0 0 2]
+function matrixPower(mat :: Matrix{Float64}, power :: Int; threaded = "none")
+    mat_pow = mat
+    for p in 2:power
+        println("Beginning power: $p")
+        mat_pow = matrixMultiply(mat, mat_pow; threaded = threaded)
+    end
+    return mat_pow
+end
+
+mat = 2 * I(1000) |> Matrix{Float64}
 println("Using ", Threads.nthreads(), " threads")
 for thread_behavior in ["none", "element", "row", "column"]
-    matrix_multiply(mat, mat; threaded=thread_behavior) |> display
+    println("Beginning thread behavior: $thread_behavior")
+    @time matrixPower(mat, 10; threaded=thread_behavior) |> display
 end
